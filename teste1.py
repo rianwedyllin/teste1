2117875e61a5912c529960e4263365efbc796056
rianwedyllin <rianwedyllin941@gmail.com>
2024-10-16 20:40:41 -0300

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+janela = tk.Tk()
+
+janela.title("Interface Gráfica com Tkinter")
+
+rotulo = tk.Label(janela, text="Bem-vindo á sua primeira interface gráfica!")
+
+rotulo.pack()
+
+janela.mainloop()
